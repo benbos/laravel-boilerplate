c52c1f17e355ffce182bf3df1fef6599fdee5e4a
Ben Bos <ben@dotlabs.nl>
2015-04-02 15:02:53 +0200

    Usemin css fixed

--- grunt/usemin.js
+++ grunt/usemin.js
@@ -8,8 +8,9 @@
 module.exports = {
     html: [ "dist/resources/views/**/*.php" ],
     js: [ "dist/public/js/*.js" ],
+    css: [ "dist/public/css/*.css" ],
     options: {
-        assetsDirs: [ "dist/public" ],
+        assetsDirs: [ "dist/public", "dist/public/img" ],
         patterns: {
             js: [
                 [ /(img\/.*?\.(?:gif|jpeg|jpg|png|webp|svg))/gm, "Update the JS to reference our revved images" ]
